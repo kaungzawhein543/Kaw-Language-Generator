b24c6fb560f2fe8ef59ab0e9c199c6af01735369
Kaung Zaw Hein <kzheindev789@gmail.com>
2025-01-14 22:06:36 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('copybutton').addEventListener('click', () => {
+    navigator.clipboard.writeText(document.getElementById('output').value);
+    alert("Copied to clipboard!");
+})
+  
